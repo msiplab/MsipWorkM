ec40fb20ae82df18637a25f0740bbabf4e20a4b3
Shogo MURAMATSU <shogo@eng.niigata-u.ac.jp>
2025-01-13 18:30:13 +0900

    arr2tex 関数にフォーマット引数を追加し、デフォルト値を設定。新しいスクリプトファイル exercise02_06.m を追加し、ヒストグラム均等化の例題を実装。

--- code/+msip/arr2tex.m
+++ code/+msip/arr2tex.m
@@ -1,9 +1,12 @@
-function y = arr2tex(x)
+function y = arr2tex(x,format)
+    if nargin < 2
+        format = "% 6.4f";
+    end
     [rows, cols] = size(x);
     y = ""+newline;
     for i = 1:rows
         for j = 1:cols
-            y = y.append(num2str(x(i, j),"% 6.4f"));
+            y = y.append(num2str(x(i, j),format));
             if j < cols
                 y = y.append(" & ");
             end
---
+++ code/scripts/ch02/exercise02_06.m
@@ -0,0 +1,33 @@
+%% 例題2.6（ヒストグラム均等化）
+% 村松正吾　「多次元信号・画像処理の基礎と展開」
+% 
+% 動作確認： MATLAB R2024b
+
+v = [
+    2 2 3 1 2 4 1 1
+    3 4 4 3 2 3 4 2
+    4 4 4 3 4 5 5 4
+    0 4 6 4 2 3 4 2
+    2 2 2 3 5 1 3 3
+    2 1 4 3 4 1 6 0
+]
+
+figure(1)
+h = histogram(v(:), 0:7);
+hv = zeros(1,8);
+hv(1:length(h.Values)) = h.Values
+
+pv = hv / numel(v);
+cv = cumsum(pv)
+
+round(cv * 7)
+
+phi = dictionary(h.BinEdges, round(cv * 7))
+
+u = phi(v)
+
+msip.arr2tex(u,"%d")
+
+figure(2)
+histogram(u(:), 0:7)
+
