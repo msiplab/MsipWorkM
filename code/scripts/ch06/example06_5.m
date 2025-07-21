4c01a8e2f67c44600aaf54c684b850408d74577f
Shogo MURAMATSU <shogo@eng.niigata-u.ac.jp>
2025-07-21 11:33:20 +0900

    DCTと基底の関係を追加

---
+++ code/scripts/ch06/example06_5.m
@@ -0,0 +1,102 @@
+close all
+iFig = 1;
+%% 
+% パラメータ設定
+
+K = 4;
+M = 2*K;
+Delta = 2/M;
+R = [-1 1]; % 表示範囲
+
+%%
+% φ のプロット
+phi = @(x) fcn_phin(x,0,K,0,0);
+
+figure(iFig)
+fplot(@(x) phi(x),R)
+grid on
+
+iFig = iFig + 1;
+
+%%
+% φn の構成
+x0 = -1;
+theta0 = 1/(2*K);
+
+phin = cell(M,1);
+for n=0:M-1
+    phin{n+1} = @(x) fcn_phin(x,n,K,x0,theta0);
+end
+
+%%
+% φn のプロット
+figure(iFig)
+
+for n=0:M-1
+    subplot(M,1,n+1)
+    fplot(@(x) phin{n+1}(x),R)
+    axis([R -1 1])
+    grid on
+end
+iFig = iFig + 1;
+
+%%
+
+% ψm の構成
+psim = cell(M,1);
+C = dctmtx(M);
+
+for m=0:M-1
+    psim{m+1} = @(x) 0;
+    for n=0:M-1
+        dnm = C(m+1,n+1);
+        psim{m+1} = @(x) psim{m+1}(x) + dnm*fcn_phin(x,n,K,x0,theta0);
+    end
+end
+
+%%
+% ψmのプロット
+figure(iFig)
+
+for m=0:M-1
+    subplot(M,1,m+1)
+    fplot(@(x) psim{m+1}(x),R)
+    axis([R -1 1])
+    grid on
+end
+iFig = iFig + 1;
+
+%% ----------------------------------------------
+%%
+function y = fcn_phin(x,n,K,x0,theta0)
+
+    y = moddiriclet( ( (x + x0) - (n/K + theta0) )/2 ,K)/(2*K);
+   
+end
+
+%%
+function y = moddiriclet(x,K)
+y = 1;
+if K > 0
+    for k=1:K-1
+        y = y + 2*cos(2*pi*k*x);
+    end
+    y = y + cos(2*pi*K*x);
+end
+end
+%%
+
+%{
+function z = chi(x,y)
+arguments (Input)
+    x
+    y
+end
+
+arguments (Output)
+    z
+end
+
+z = (x>=0).*(x<y);
+end
+%}
