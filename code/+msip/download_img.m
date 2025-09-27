ad18fbfd09e336cf7b2a13330ffebd7d27da8058
Shogo MURAMATSU <shogo@eng.niigata-u.ac.jp>
2025-09-28 07:08:01 +0900

    サンプル画像を変更

--- code/+msip/download_img.m
+++ code/+msip/download_img.m
@@ -1,7 +1,7 @@
 function download_img(isVerbose)
 % DOWNLOAD_IMG 
 %
-% Copyright (c) Shogo MURAMATSU, 2018
+% Copyright (c) Shogo MURAMATSU, 2018-2023
 % All rights reserved.
 %
 
@@ -13,13 +13,14 @@ end
 % 画像サンプルのダウンロード
 dstdir = './data/';
 if exist(dstdir,'dir') == 7
-    fnames = {'lena' 'baboon' 'goldhill' 'barbara'};
-    for idx = 1:length(fnames)
-        fname = [ fnames{idx} '.png' ];
+    %fnames = {'lena' 'baboon' 'goldhill' 'barbara'};
+    for idx = 1:24 %length(fnames)
+        fname = "kodim"+num2str(idx,'%02d') + ".png"; % [ fnames{idx} '.png' ]
         if exist(fullfile(dstdir,fname),'file') ~= 2
             img = imread(...
-                sprintf('http://homepages.cae.wisc.edu/~ece533/images/%s',...
-                fname));
+                ...sprintf('http://homepages.cae.wisc.edu/~ece533/images/%s',...
+                ...fname));
+                "https://www.r0k.us/graphics/kodak/kodak/"+fname);                
             imwrite(img,fullfile(dstdir,fname))
             if isVerbose
                 fprintf('Downloaded and saved %s in %s\n',fname,dstdir);
@@ -30,6 +31,7 @@ if exist(dstdir,'dir') == 7
             end
         end
     end
+    disp('See <a href="http://www.r0k.us/graphics/kodak/">Kodak Lossless True Color Image Suite</a>')
 else
     me = MException('MsipException:NoSuchFolder', ...
         '%s folder does not exist',dstdir);
