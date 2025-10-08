6440889a0ed38a9f21f205a143e4621f33e3589f
Shogo MURAMATSU <shogo@eng.niigata-u.ac.jp>
2025-10-08 10:58:47 +0900

    因数分解を確認

--- code/scripts/ch06/example06_17.m
+++ code/scripts/ch06/example06_17.m
@@ -2,11 +2,17 @@ waveinfo('db')
 
 % DB1 (Haar)
 figure(1)
-[~,~,f0,~] = wfilters('db1')
-zplane(f0)
+[~,~,f0,f1] = wfilters('db1')
+subplot(1,2,1), zplane(f0)
+subplot(1,2,2), zplane(f1)
 
 % DB2
 figure(2)
-[~,~,f0,~] = wfilters('db2')
-zplane(f0)
+[~,~,f0,f1] = wfilters('db2')
+subplot(1,2,1), zplane(f0)
+subplot(1,2,2), zplane(f1)
+
+%%
+2^(-5/2)*conv(conv([1 1],[1 1]),[1+sqrt(3) 1-sqrt(3)])
+2^(-5/2)*conv(conv([1 -1],[1 -1]),[1-sqrt(3) -1-sqrt(3)])
 
