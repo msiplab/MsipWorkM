a493560dba10c119602f517c6d45678ad74434b4
Shogo MURAMATSU <shogo@eng.niigata-u.ac.jp>
2026-02-03 07:05:04 +0900

    例1.3および例1.4の内容を更新し、例1.5を削除して新たに例1.xを追加

--- code/scripts/ch01/example01_03.m
+++ code/scripts/ch01/example01_03.m
@@ -1,4 +1,4 @@
-%[text] # 例1.3（線形写像と行列表現）
+%[text] # 例1.3（静止画像のデータ量）
 %[text] 村松正吾　「多次元信号・画像処理の基礎と展開」
 %[text] 動作確認： MATLAB R2025b
 %[text] ## \\$
@@ -6,23 +6,27 @@ N1 = 2304
 N2 = 3456
 N = N1 * N2
 
-%[text] 8ビット符号なし整数型
+%%
+%[text] ## (1) 8ビット符号なし整数型（\\$\\beta=8\\$\[bits\]）のグレースケール画像の場合
 beta = 8;
 B = beta*N1*N2;
 fprintf("8ビット符号なし整数型\n");
 fprintf(" ビット数 = %d bits\n", B);
 fprintf(" バイト数 = %d bytes\n", B/8);
 I=zeros(N1,N2,'uint8');
-whos I
+dataInfo = whos('I');
+disp(dataInfo)
 
-%[text] 倍精度実数型
+%%
+%[text] ## (2) 倍精度実数型（\\$\\beta=8\\$\[bits\]）のRGB画像の場合
 beta = 64;
 B = 3*beta*N1*N2;
 fprintf("倍精度実数型\n");
 fprintf(" ビット数 = %d bits\n", B);
 fprintf(" バイト数 = %d bytes\n", B/8);
 J=zeros(N1,N2,3,'double');
-whos J
+dataInfo = whos('J');
+disp(dataInfo)
 
 %[appendix]{"version":"1.0"}
 %---
---
+++ code/scripts/ch01/example01_0x.m
@@ -0,0 +1,20 @@
+%[text] # 例1.x（ベクトル化）
+%[text] 村松正吾　「多次元信号・画像処理の基礎と展開」
+%[text] 動作確認： MATLAB R2025b
+%[text] ## $2\\times 3$配列の生成
+V = rand(2,3)
+size(V)
+%%
+%[text] ## 列ベクトル化
+v = V(:)
+size(v)
+%%
+%[text] ## 逆列ベクトル化
+U = reshape(v,2,3)
+size(U)
+
+%[appendix]{"version":"1.0"}
+%---
+%[metadata:view]
+%   data: {"layout":"onright"}
+%---
