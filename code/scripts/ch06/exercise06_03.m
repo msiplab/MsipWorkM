604e1bb2a18111e9f3bfff57cd5749b4817213a5
Shogo MURAMATSU <shogo@eng.niigata-u.ac.jp>
2026-03-31 06:29:50 +0900

    例題のコードを更新

--- code/scripts/ch06/exercise06_03.m
+++ code/scripts/ch06/exercise06_03.m
@@ -1,20 +1,27 @@
-X = [
-6 5 3 2
-6 5 3 2
-2 3 5 6
-2 3 5 6
-]
+%%
+%X = [
+%6 5 3 2 
+%6 5 3 2
+%2 3 5 6
+%2 3 5 6
+%]
+X= [
+  5  5  7  6  7  2;
+  6  2  3  2  7  2;
+  2  1  5  4  4  6;
+  5  4  2  5  1  2;
+  ]
 C = dctmtx(2)
 
 Y = blockproc(X,[2 2],@(X) C*X.data*C.')
 
 
-msip.arr2tex(Y)
+msip.arr2tex(Y,"%.1f")
 
 
 R = blockproc(Y,[2 2],@(Y) C.'*Y.data*C)
 
-msip.arr2tex(R)
+msip.arr2tex(R,"%.0f")
 
 %%
 A1 = rand(2)
