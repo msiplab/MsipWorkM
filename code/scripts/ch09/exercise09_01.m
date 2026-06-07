0a7d58eb1de6e3ba6afde356b8f9469da3a7f1e3
Shogo MURAMATSU <shogo@eng.niigata-u.ac.jp>
2026-06-08 08:02:38 +0900

    例題9.1のコードを更新

--- code/scripts/ch09/exercise09_01.m
+++ code/scripts/ch09/exercise09_01.m
@@ -1,29 +1,37 @@
-X = [ 6 5 3 2
-    6 5 3 2
-    2 3 5 6
-    2 3 5 6];
-
-% X: (2*p) x (2*q)
+%% 例題9.1（k-平均値法）
+% 村松正吾　「多次元信号・画像処理の基礎と展開」
+% 
+% 動作確認： MATLAB R2026a
+X= [
+    5.0000    5.0000    7.0000    6.0000    7.0000    2.0000
+    6.0000    2.0000    3.0000    2.0000    7.0000    2.0000
+    2.0000    2.0000    4.0000    4.0000    4.0000    6.0000
+    4.0000    4.0000    4.0000    4.0000    1.0000    2.0000
+  ]
+% サイズとブロック数
 [m,n] = size(X);
 p = m/2;
 q = n/2;
 
 % 得られる B は 2 x 2 x (p*q)
-B = reshape(X, 2, p, 2, q);   % -> 2 x p x 2 x q
-B = permute(B, [1 3 2 4]);    % -> 2 x 2 x p x q
-B = reshape(B, 2, 2, p*q);    % -> 2 x 2 x (p*q)
+Xb = reshape(X, 2, p, 2, q);   % -> 2 x p x 2 x q
+Xb = permute(Xb, [1 3 2 4]);   % -> 2 x 2 x p x q
+Xb = reshape(Xb, 2, 2, p*q);   % -> 2 x 2 x (p*q)
 
 % k-means 初期化
-c(:,:,1) = 2*ones(2);
-c(:,:,2) = 6*ones(2);
+c(:,:,1) = min(X(:))*ones(2)
+c(:,:,2) = max(X(:))*ones(2)
 
-for iter = 1:2
+nIter = 5;
+B=size(Xb,3);
+
+for iter = 1:nIter
     % クラスタリング
     I = cell(1, 2); % Initialize cell array for cluster indices
-    for idx = 1:4
-        e1 = norm(B(:,:,idx)-c(:,:,1),'fro');
-        e2 = norm(B(:,:,idx)-c(:,:,2),'fro');
-        if e1 < e2
+    for idx = 1:B
+        e1 = norm(Xb(:,:,idx)-c(:,:,1),'fro');
+        e2 = norm(Xb(:,:,idx)-c(:,:,2),'fro');
+        if e1 <= e2
             I{1} = [I{1} idx];
         else
             I{2} = [I{2} idx];
@@ -32,7 +40,18 @@ for iter = 1:2
     I
 
     % コードベクトルの更新
-    c(:,:,1) = mean(B(:,:,I{1}),3)
-    c(:,:,2) = mean(B(:,:,I{2}),3)
+    c(:,:,1) = mean(Xb(:,:,I{1}),3);
+    c(:,:,2) = mean(Xb(:,:,I{2}),3);
 end
+c
 
+for icls = 1:2
+    for idx = I{icls}
+        Yb(:,:,idx) = c(:,:,icls);
+    end
+end
+Yb = reshape(Yb,2,2,p,q);
+Yb = ipermute(Yb,[1 3 2 4]);
+Y = reshape(Yb,2*p,2*q);
+
+msip.arr2tex(Y,"%4.1f")
