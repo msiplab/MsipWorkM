8295730c067833e6437386f6944e6fb537357199
Shogo MURAMATSU <shogo@eng.niigata-u.ac.jp>
2026-06-20 11:04:05 +0900

    SaivDr パッケージのセットアップ関数を追加

---
+++ code/+msip/saivdrSetup.m
@@ -0,0 +1,26 @@
+function setup(isCodegen)
+%
+if nargin < 1
+    isCodegen = true;
+end
+
+% SaivDr パッケージバージョン
+SAIVDR_VER = "4.2.2.4";
+codedir = fileparts(fileparts(mfilename('fullpath')));
+SAIVDR_DIR = fullfile(codedir, "SaivDr-"+SAIVDR_VER);
+if ~exist(SAIVDR_DIR,"dir")
+    unzip("https://github.com/msiplab/SaivDr/archive/refs/tags/"+SAIVDR_VER+".zip", codedir)
+else
+    disp(SAIVDR_DIR+" exits.")
+end
+ccd = cd(SAIVDR_DIR);
+setpath
+if ~isCodegen
+    disp("Skip code generation")
+elseif isempty(dir("./mexcodes/fcn_*"))
+    mybuild
+else
+    disp("MEX files exist.")
+end
+cd(ccd)
+end
